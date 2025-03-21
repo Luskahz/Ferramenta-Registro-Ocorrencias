c4cdb87399ef0c67cc7b718bc5789589d9d0d432
Luskahz <lucasalvesdemelolins@gmail.com>
2025-03-21 00:59:07 -0300

    adcionando o tailwind

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import GlobalHeader from "./GlobalHeader";
 import SideBarLeft from "./sideBarLeft";
 import SideBarRight from "./sideBarRight";
+import "../index.css"
 
 
 
