eb4b640d754999fdb492822bcb3efd19344846b9
Luskahz <lucasalvesdemelolins@gmail.com>
2025-03-24 00:12:15 -0300

    atualização css globalheader

--- src/components/GlobalHeader.jsx
+++ src/components/GlobalHeader.jsx
@@ -1,28 +1,22 @@
-import React from "react"
+import React from "react";
 
+const GlobalHeader = ({ children }) => {
+  return (
+    <>
+      <header className="absolute z-10 flex h-[var(--GlobalHeaderH)] w-full flex-row items-center justify-between bg-gray-900">
+        <div className="start-itens m-2 flex h-[50%] w-[40%] flex-row items-center gap-5 bg-gray-900">
+          <div className="side-menu-button flex h-full w-[2rem] items-center justify-center border-1 text-white">&#9776;</div>
+          <div className="logo-icon-button h-full w-[2rem] bg-gray-700"></div>
+          <div className="Breadcrumb h-full min-w-[2rem] bg-gray-700"></div>
+        </div>
+        <div className="end-itens m-2 flex h-[50%] w-[50%] flex-row-reverse gap-5 bg-gray-900">
+          <div className="profile-menu-button h-full w-[2rem] bg-gray-700">P</div>
+          <nav className="navbar h-full min-w-[2rem] bg-gray-700">{children}</nav>
+          <div className="search-icon-button h-full w-[4rem] bg-gray-700">S</div>
+        </div>
+      </header>
+    </>
+  );
+};
 
-
-const GlobalHeader = ({children}) => {
-    return (
-        <>
-        <header className="absolute justify-between items-center w-full h-[var(--GlobalHeaderH)] z-10 bg-gray-900 flex flex-row ">
-            <div className="border-2 start-itens w-[40%] m-2 h-[calc(var(--GlobalHeaderH)-3vh)] bg-gray-900 flex flex-row items-center gap-5 ">
-                <div className="side-menu-button w-[5%] h-full text-white border-1 flex items-center justify-center" >&#9776;</div>
-                <div className="logo-icon-button w-[20px] h-full bg-amber-600">a</div>
-                <div className="context-icon-button w-[20px] h-full bg-amber-900">a</div>
-            </div>
-            <div className="border-2 end-itens w-[40%] m-2 h-[calc(var(--GlobalHeaderH)-3vh)] bg-gray-900 flex flex-row">
-                <div className="profile-menu-button w-[20px] h-full bg-orange-200"></div>
-                <nav className="bg-amber-800 h-full w-[200px]">
-                    {children}
-                </nav>
-                <div className="search-icon-button w-[20px] h-full bg-amber-700"></div>
-            </div>
-
-        </header>
-        </>
-    )
-}
-
-
-export default GlobalHeader
+export default GlobalHeader;
