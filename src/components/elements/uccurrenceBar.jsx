38976b9c0b9df48c8d7f6088bb7fe836dd56d4ba
Luskahz <lucasalvesdemelolins@gmail.com>
2025-04-22 00:55:04 -0300

    ajuste responsividade

--- src/components/elements/uccurrenceBar.jsx
+++ src/components/elements/uccurrenceBar.jsx
@@ -3,12 +3,12 @@ import { UccurrenceCard, Title } from "../index";
 export default function UccurrenceBar({ setHover, hover }) {
   return (
     <div
-      className={`Occurrences z-0 w-full border-t-3 border-[var(--gray-80)] bg-[var(--gray-90)] pl-3 ${hover ? "h-[12rem]" : "h-[12vh] sm:h-[6vh] "} absolute bottom-0 transition-all duration-500`}
+      className={`Occurrences z-0 w-full border-t-3 border-[var(--gray-80)] bg-[var(--gray-90)] pl-3 ${hover ? "h-[24rem] sm:h-[12rem]" : "h-[12vh] sm:h-[6vh] "} absolute bottom-0 transition-all duration-500`}
       onMouseEnter={() => setHover(true)}
       onMouseLeave={() => setHover(false)}
     >
       <Title.Md_L className={"p-1.5 pb-6"}>Ocorrencias</Title.Md_L>
-      <div className="OccurrencesList scrollbar-custom flex gap-5 overflow-x-auto">
+      <div className="OccurrencesList scrollbar-custom flex gap-5 overflow-x-auto pb-3">
         <UccurrenceCard></UccurrenceCard>
         <UccurrenceCard></UccurrenceCard>
         <UccurrenceCard></UccurrenceCard>
